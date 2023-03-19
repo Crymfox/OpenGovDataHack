073e7a80f7d75d7c5eebddd06fa7846275d49a0d
Nie-Mand <chill.out.x19@gmail.com>
2023-03-19 04:20:56 +0100

    quick fix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,10 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
