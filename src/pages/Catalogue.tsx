41dfb7f5f9f1951e7e474f7bea2657a7eb2e94b8
Nie-Mand <chill.out.x19@gmail.com>
2023-03-19 06:45:10 +0100

    catalogue

--- src/pages/Catalogue.tsx
+++ src/pages/Catalogue.tsx
@@ -2,18 +2,21 @@ import QuizCard from "../components/QuizCard"
 
 const Catalogue = () => {
     return (
-        <div className="mx-16 my-8">
-            <h1 className="text-5xl text-slate-800 m-20 text-center">Pack of Quiz:</h1>
-            <div className="flex flex-wrap">
-                <QuizCard />
-                <QuizCard />
-                <QuizCard />
-                <QuizCard />
-                <QuizCard />
-                <QuizCard />
-            </div>
+      <div className="mx-16 my-8">
+        <h1 className="text-5xl text-slate-800 m-20 text-center">Packs:</h1>
+        <div className="grid grid-cols-3 gap-20">
+          <QuizCard
+            title="Is Computer Science right for me?"
+            description="This quiz will help you determine if computer science is the right major for you."
+          />
+
+          <QuizCard
+            title="Am I into Robotics?"
+            description="This quiz will help you determine if you should pursue a career in robotics."
+          />
         </div>
-    )
+      </div>
+    );
 }
 
 export default Catalogue
