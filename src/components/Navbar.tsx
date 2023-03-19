4d6e31775c1e4dabe9ef69121924dffaa2efd0ce
Nie-Mand <chill.out.x19@gmail.com>
2023-03-19 04:22:41 +0100

    links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,29 @@
-const Navbar = () => {
-    return (
-        <div className="flex flex-wrap px-3 bg-blue-800 text-white text-lg font-semibold">
-            <a href="" className="p-1">
-                <img src="/assets/Logo.png" alt="Logo" width={150} />
-            </a>
-            <div className="ml-auto"></div>
-            <a href="" className="p-4 hover:bg-blue-900 duration-300">Catalogue</a>
-            <a href="" className="p-4 hover:bg-blue-900 duration-300">University</a>
-            <a href="" className="p-4 hover:bg-blue-900 duration-300">School</a>
-            <a href="" className="p-4 hover:bg-blue-900 duration-300">Share</a>
-            <a href="" className="p-4 hover:bg-blue-900 duration-300">Partners</a>
-        </div>
-    )
-}
+import { Link } from "react-router-dom";
 
-export default Navbar
+const Navbar = () => {
+  return (
+    <div className="flex flex-wrap px-3 bg-blue-800 text-white text-lg font-semibold">
+      <Link to="/" className="p-1">
+        <img src="/assets/Logo.png" alt="Logo" width={150} />
+      </Link>
+      <div className="ml-auto"></div>
+      <Link to="/catalogue" className="p-4 hover:bg-blue-900 duration-300">
+        Catalogue
+      </Link>
+      <Link to="/universities" className="p-4 hover:bg-blue-900 duration-300">
+        University
+      </Link>
+      <Link to="/schools" className="p-4 hover:bg-blue-900 duration-300">
+        School
+      </Link>
+      <Link to="/share" className="p-4 hover:bg-blue-900 duration-300">
+        Share
+      </Link>
+      <Link to="/partners" className="p-4 hover:bg-blue-900 duration-300">
+        Partners
+      </Link>
+    </div>
+  );
+};
+
+export default Navbar;
